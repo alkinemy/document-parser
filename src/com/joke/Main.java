643117e8a6b1d41b00573e27d3ae013a710f6349
joke111 <alta.second@gmail.com>
2015-12-10 12:18:31 +0900

    Remote class files && Implement DOM Parser

--- src/com/joke/Main.java
+++ src/com/joke/Main.java
@@ -1,10 +1,14 @@
 package com.joke;
 
+import com.joke.parser.DomParser;
+
+import java.io.File;
+
 public class Main {
 
     public static void main(String[] args) {
-//		DomParser parser = new DomParser();
+		DomParser parser = new DomParser();
 
-//		parser.parse(new File("/Users/joke/Downloads/World.xml"));
+		parser.findElements(new File("/Users/joke/Downloads/World.xml"), "item");
 	}
 }
--- src/com/joke/parser/DomParser.java
+++ src/com/joke/parser/DomParser.java
@@ -8,13 +8,18 @@ public class DomParser implements DocumentParser {
 	//전부 메모리에 읽어서 객체로 만들어놓고 처리하는듯
 	//트리를 미리 만드는듯 -> 트리를 따라가면서 필요한 정보를 얻거나 출력
 
-	public Node findElement(File file, String elementName) {
+	public Node findElements(File file, String elementName) {
 		Node document = parse(file);
-		return findElement(elementName);
+		return findElements(document, elementName);
 	}
 
-	private Node findElement(String elementName) {
+	private Node findElements(Node document, String elementName) {
 		return null;
+	}
+
+	private String removeLast(String origin, String pattern) {
+		int lastIndex = origin.lastIndexOf(pattern);
+		return origin.substring(0, lastIndex) + origin.substring(lastIndex + pattern.length(), origin.length() - 1);
 	}
 
 	private Node parse(File file) {
@@ -24,33 +29,38 @@ public class DomParser implements DocumentParser {
 		try(Scanner scanner = new Scanner(file)) {
 			scanner.useDelimiter("<");
 			while(scanner.hasNext()) {
-				String element = scanner.next();
-
-				if (element.isEmpty()) {
+				String elementLine = scanner.next();
+				if (elementLine.isEmpty()) {
 					continue;
 				}
 
-				if (isSelfCloseElement(element)) {
-					//create child
-					//pop parent
-					//add child
-					//push parent
-					//push child
-				} else if (isOpenElement(element)) {
-					//create node
-					//if it has value, set value
-					//push node
-				} else if (isCloseElement(element)) {
-					//pop node
+				if (isSelfCloseElement(elementLine)) {
+					Element element = Element.parseSelfCloseElement(elementLine);
+					Node child = new Node(element);
+					nodes.peek().addChild(child);
+				} else if (isCloseElement(elementLine)) {
+					String elementName = elementLine.replaceFirst("/", "").replaceFirst(">", "").trim();
+					Node node = nodes.pop();
+					if (!node.getName().equals(elementName)) {
+						System.out.println(elementLine);
+						throw new RuntimeException("Invalid XML file");
+					}
+				} else if (isOpenElement(elementLine)) {
+					Element element = Element.parseOpenElement(elementLine);
+					Node child = new Node(element);
+					nodes.peek().addChild(child);
+					nodes.push(child);
 				} else {
 					//???
+					System.out.println("I don't know... ??????");
 				}
-				System.out.println(element);
 			}
 		} catch (FileNotFoundException e) {
 			e.printStackTrace();
+			return null;
 		}
-		return null;
+
+		return nodes.pop();
 	}
 
 	private boolean isSelfCloseElement(String element) {
@@ -62,15 +72,20 @@ public class DomParser implements DocumentParser {
 	}
 
 	private boolean isCloseElement(String element) {
-		return element.startsWith("/") && element.endsWith(">");
+		return element.startsWith("/") && element.contains(">");
 	}
 
 	public static class Node {
 		private List<Node> children;
+
 		private Element element;
 
 		public Node(String name) {
 			this.element = new Element(name);
+		}
+
+		public Node(Element element) {
+			this.element = element;
 		}
 
 		public List<Node> getChildren() {
@@ -91,6 +106,10 @@ public class DomParser implements DocumentParser {
 		public void setValue(String value) {
 			element.setValue(value);
 		}
+
+		public String getName() {
+			return element.getName();
+		}
 	}
 
 	public static class Element {
@@ -102,6 +121,48 @@ public class DomParser implements DocumentParser {
 
 		public Element(String name) {
 			this.name = name;
+		}
+
+		public static Element parseSelfCloseElement(String raw) {
+			String elementLine = null;
+			if (raw.startsWith("?") && raw.contains("?>")) {
+				int lastIndex = raw.lastIndexOf("?>");
+				elementLine = raw.substring(0, lastIndex + 2);
+				elementLine = elementLine.replaceFirst("\\?", "").replaceFirst("\\?>", "").trim();
+			} else if (raw.contains("/>")) {
+				int lastIndex = raw.lastIndexOf("/>");
+				elementLine = raw.substring(0, lastIndex + 2);
+				elementLine = elementLine.replaceFirst("/>", "").trim();
+			} else {
+				System.out.println(raw);
+				throw new RuntimeException("????");
+			}
+			return makeElementWithAttributes(elementLine);
+		}
+
+		public static Element parseOpenElement(String raw) {
+			String[] elementPart = raw.split(">");
+			Element element = makeElementWithAttributes(elementPart[0]);
+			if (elementPart.length == 2) {
+				element.setValue(elementPart[1]);
+			}
+			return element;
+		}
+
+		private static Element makeElementWithAttributes(String elementLine) {
+			String[] attributes = elementLine.split(" ");
+			Element element = new Element(attributes[0]);
+			for(int i = 1; i < attributes.length; i++) {
+				String[] keyValue = attributes[i].split("=");
+				String key = keyValue[0];
+				if (keyValue[1].length() > 2) {
+					String value = keyValue[1].substring(1, keyValue[1].length() - 2);
+					element.addAttribute(key, value);
+				} else {
+					element.addAttribute(key, keyValue[1]);
+				}
+			}
+			return element;
 		}
 
 		public String getName() {
